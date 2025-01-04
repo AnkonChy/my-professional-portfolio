bc8405bb61f219ad8c91e5ee2c30defa7843701e
Ankon Chowdhury <ankonchy1@gmail.com>
2025-01-04 21:47:59 +0600

    all setup & configuration done also navbar section completed with responsive

---
+++ src/mainLayout/MainLayout.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar/Navbar";
+
+const MainLayout = () => {
+  return <div className="md:w-10/12 md:mx-auto font-montserrat">
+    <Navbar></Navbar>
+  </div>;
+};
+
+export default MainLayout;
